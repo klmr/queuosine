44abad46bc3c994cf92e0f1ad41a7e0fb41f01ed
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-06-27 18:48:54 +0100

    Replace link to code with copy

--- code/cds.r
+++ code/cds.r
@@ -1,1 +1,10 @@
-/Users/konrad/Projects/Alper/mcm5s2U/code/cds.r
+cu = modules::import('klmr/codons/codon_usage')
+
+valid_cds = function (cds) {
+    n = nchar(cds)
+    n > 0 &
+        cds != 'Sequence unavailable' &
+        n %% 3 == 0 &
+        substr(cds, 1, 3) == 'ATG' &
+        substr(cds, n - 2, n) %in% cu$stop_codons
+}
